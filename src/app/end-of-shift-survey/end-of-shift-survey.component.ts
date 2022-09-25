a66ae8c2ee04f23f8e9c69c7b19caf10365f14f3
Arisendust34 <68385227+Arisendust34@users.noreply.github.com>
2022-09-25 14:01:12 -0400

    init

---
+++ src/app/end-of-shift-survey/end-of-shift-survey.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-end-of-shift-survey',
+  templateUrl: 'end-of-shift-survey.component.html',
+  styleUrls: ['end-of-shift-survey.component.css']
+})
+export class ContactFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
