1011d870499e023cbcbe82eac5b9e7aa4017bbd4
Arisendust34 <68385227+Arisendust34@users.noreply.github.com>
2022-10-10 17:44:59 -0400

    init

--- src/app/login-page/login-page-main.component.ts
+++ src/app/login-page/login-page-main.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['login-page-main.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  
 
   constructor() { }
 
