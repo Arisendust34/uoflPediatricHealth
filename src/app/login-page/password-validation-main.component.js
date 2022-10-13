40bf6b4d301b9afcedc5a14289790747f3ecd418
Michael Martin <ss013r@gmail.com>
2022-10-13 01:22:04 -0400

    Removing now-unused files

--- src/app/login-page/password-validation-main.component.js
+++
@@ -1,9 +0,0 @@
-function onChange() {
-    const password = document.getElementById('password1');
-    const confirm = document.getElementById('password2');
-    if (confirm.value === password.value) {
-      confirm.setCustomValidity('');
-    } else {
-      confirm.setCustomValidity('Passwords do not match');
-    }
-  }
