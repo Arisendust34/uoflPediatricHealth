fb7c61ee2241a7ed3362f148eceeac63b0f28bfe
billy <82614142+billyt01@users.noreply.github.com>
2022-11-22 21:10:29 -0500

    Update button.py

--- button.py
+++ button.py
@@ -1,8 +1,33 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
 
-button1 = Button(root, text="User 1")
-button1.pack()
+def click():
+    print("User")
 
-root.mainLoop()
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button1 = tk.Button(frame,
+                   text="User 1",
+                   fg="red",
+                   command=click)
+button1.pack(side=tk.LEFT)
+button2 = tk.Button(frame,
+                   text="User 2",
+                   command=click)
+button2.pack(side=tk.LEFT)
+button3 = tk.Button(frame,
+                   text="User 3",
+                   command=click)
+button3.pack(side=tk.LEFT)
+button4 = tk.Button(frame,
+                   text="User 4",
+                   command=click)
+button4.pack(side=tk.LEFT)
+buttonq = tk.Button(frame,
+                   text="Home",
+                   command=quit)
+buttonq.pack(side=tk.LEFT)
+root.mainloop()
