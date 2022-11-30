7dc4c7fc0b014765077fbf34f5a919cc19aaa7be
Michael Martin <ss013r@gmail.com>
2022-11-30 09:05:45 -0500

    Employee List: converted JS to TS

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -11,4 +11,24 @@ export class EmployeeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  //Search the HTML Table for matching string
+  employeeListSearch() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = <HTMLInputElement>document.getElementById('myInput'); //Must cast. User's input in text field
+    filter = input.value.toUpperCase();
+    ul = <HTMLInputElement>document.getElementById('myUL'); //Must cast (same for all following vars)
+    li = <HTMLCollectionOf<HTMLLIElement>>document.getElementsByTagName('li'); //Casted to HTMLCollectionOf<HTMLLIElement>
+    a = <HTMLInputElement>document.getElementById('myTable'); 
+    txtValue = <HTMLInputElement>document.getElementById('myTable'); 
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
 }
